c55dab8cd99a6ee37208dbe1832e427da2890250
Aman Bansal <18bansal.aman@gmail.com>
2023-02-26 21:28:32 -0500

    use numpy black magic to make code run faster

--- A1/python_starter/basicReconstruction.py
+++ A1/python_starter/basicReconstruction.py
@@ -2,7 +2,8 @@ import argparse
 import numpy as np
 from skimage import measure
 from sklearn.neighbors import KDTree
-import open3d as o3d;
+import open3d as o3d
+
 
 def createGrid(points, resolution=96):
     """
@@ -16,16 +17,22 @@ def createGrid(points, resolution=96):
         X,Y,Z coordinates of grid vertices     
         max and min dimensions of the bounding box of the point cloud                 
     """
-    max_dimensions = np.max(points,axis=0) # largest x, largest y, largest z coordinates among all surface points
-    min_dimensions = np.min(points,axis=0) # smallest x, smallest y, smallest z coordinates among all surface points    
-    bounding_box_dimensions = max_dimensions - min_dimensions # com6pute the bounding box dimensions of the point cloud
-    max_dimensions = max_dimensions + bounding_box_dimensions/10  # extend bounding box to fit surface (if it slightly extends beyond the point cloud)
+    max_dimensions = np.max(
+        points, axis=0)  # largest x, largest y, largest z coordinates among all surface points
+    # smallest x, smallest y, smallest z coordinates among all surface points
+    min_dimensions = np.min(points, axis=0)
+    # com6pute the bounding box dimensions of the point cloud
+    bounding_box_dimensions = max_dimensions - min_dimensions
+    # extend bounding box to fit surface (if it slightly extends beyond the point cloud)
+    max_dimensions = max_dimensions + bounding_box_dimensions/10
     min_dimensions = min_dimensions - bounding_box_dimensions/10
-    X, Y, Z = np.meshgrid( np.linspace(min_dimensions[0], max_dimensions[0], resolution),
-                           np.linspace(min_dimensions[1], max_dimensions[1], resolution),
-                           np.linspace(min_dimensions[2], max_dimensions[2], resolution) )    
-    
+    X, Y, Z = np.meshgrid(np.linspace(min_dimensions[0], max_dimensions[0], resolution),
+                          np.linspace(
+        min_dimensions[1], max_dimensions[1], resolution),
+        np.linspace(min_dimensions[2], max_dimensions[2], resolution))
+
     return X, Y, Z, max_dimensions, min_dimensions
+
 
 def sphere(center, R, X, Y, Z):
     """
@@ -36,9 +43,11 @@ def sphere(center, R, X, Y, Z):
         X,Y,Z : coordinates of grid vertices                      
     Returns: 
         IF    : implicit function of the sphere sampled at the grid points
-    """    
-    IF = (X - center[0]) ** 2 + (Y - center[1]) ** 2 + (Z - center[2]) ** 2 - R ** 2 
+    """
+    IF = (X - center[0]) ** 2 + (Y - center[1]) ** 2 + \
+        (Z - center[2]) ** 2 - R ** 2
     return IF
+
 
 def showMeshReconstruction(IF):
     """
@@ -46,17 +55,18 @@ def showMeshReconstruction(IF):
     and shows the reconstruction mesh
     Args:
         IF    : implicit function sampled at the grid points
-    """    
-    verts, triangles, normals, values = measure.marching_cubes(IF, 0)        
+    """
+    verts, triangles, normals, values = measure.marching_cubes(IF, 0)
 
     # Create an empty triangle mesh
     mesh = o3d.geometry.TriangleMesh()
-    # Use mesh.vertex to access the vertices' attributes    
+    # Use mesh.vertex to access the vertices' attributes
     mesh.vertices = o3d.utility.Vector3dVector(verts)
-    # Use mesh.triangle to access the triangles' attributes    
+    # Use mesh.triangle to access the triangles' attributes
     mesh.triangles = o3d.utility.Vector3iVector(triangles.astype(np.int32))
-    mesh.compute_vertex_normals()        
-    o3d.visualization.draw_geometries([mesh])   
+    mesh.compute_vertex_normals()
+    o3d.visualization.draw_geometries([mesh])
+
 
 def mlsReconstruction(points, normals, X, Y, Z):
     """
@@ -65,8 +75,8 @@ def mlsReconstruction(points, normals, X, Y, Z):
     The method shows reconstructed mesh
     Args:
         points :  points of the point cloud
-		normals:  normals of the point cloud
-		X,Y,Z  :  coordinates of grid vertices 
+                normals:  normals of the point cloud
+                X,Y,Z  :  coordinates of grid vertices 
     Returns:
         IF     :  implicit function sampled at the grid points
     """
@@ -74,66 +84,44 @@ def mlsReconstruction(points, normals, X, Y, Z):
     ################################################
     # <================START CODE<================>
     ################################################
-     
-    ##Calculating estimate of beta using KDTree
-    
+
+    # Calculating estimate of beta using KDTree
+
     Q = np.array([X.reshape(-1), Y.reshape(-1), Z.reshape(-1)]).transpose()
-    print(Q.shape, points.shape)
     tree = KDTree(points)
     _, idx = tree.query(points, k=2)
-    beta = 2*np.mean(_[:,1:].reshape(-1))
-    print("beta------>)",beta)
-    ### beta estimate complete
-
+    beta = 2*np.mean(_[:, 1:].reshape(-1))
+    # beta estimate complete
 
     # replace this random implicit function with your MLS implementation!
     IF = np.random.rand(X.shape[0], X.shape[1], X.shape[2]) - 0.5
 
-
-    shapeX = X.shape    
+    shapeX = X.shape
     Q = np.array([X.reshape(-1), Y.reshape(-1), Z.reshape(-1)]).transpose()
-    # print(Q.shape)
-    # print("**********",Q.shape)
-    # print(X,Q)
     tree = KDTree(points)
 
-    _, idx = tree.query(Q, k=50)  
-    # print(idx,idx.reshape(shapeX))
-    idxs = idx.reshape(shapeX[0],shapeX[1],shapeX[2],-1)
-    print(idxs)
-    for _ in range(len(idxs)):
-        for __ in range(len(idxs)):
-            for ___ in range(len(idxs)):
-                x = X[_][__][___]
-                y = Y[_][__][___]
-                z = Z[_][__][___]
-                p = [x,y,z]
-                idxt = idxs[_][__][___]
-                runningPhi = 0.0
-                runningNumerator = 0.0
-                for iddx in idxt:
-                    p_i = points[iddx]
-                    tv = np.linalg.norm(p-p_i)
-                    d_ip = tv*(normals[iddx]@(p-p_i))
-                    phi_ip = np.exp(-(tv*tv)/(beta*beta))
-                    runningPhi += phi_ip
-                    runningNumerator += phi_ip*d_ip
-                IF[_][__][___] = runningNumerator/runningPhi
+    _, idx = tree.query(Q, k=50)
 
+    IF = IF.reshape(-1)
+    for i, fiftyIndices in enumerate(idx):
+        p = Q[i]
+        pts = points[fiftyIndices]
+        ppts = p - pts
+        nrmls = normals[fiftyIndices]
+        dip = np.sum(nrmls*ppts, axis=1)
+        phii = np.linalg.norm(ppts, axis=1)
+        phii = phii*phii
+        phii = phii/(-(beta*beta))
+        phii = np.exp(phii)
+        IF[i] = np.sum(phii*dip, axis=0)/np.sum(phii, axis=0)
 
-    # this is an example of a kd-tree nearest neighbor search (adapt it accordingly for your task)
-	# use kd-trees to find nearest neighbors efficiently!
-	# kd-tree: https://en.wikipedia.org/wiki/K-d_tree
-    # Q = np.array([X.reshape(-1), Y.reshape(-1), Z.reshape(-1)]).transpose()
-    # tree = KDTree(points)
-    # _, idx = tree.query(Q, k=2)  
-	
+    IF = IF.reshape(shapeX)
 
     ################################################
     # <================END CODE<================>
     ################################################
 
-    return IF 
+    return IF
 
 
 def naiveReconstruction(points, normals, X, Y, Z):
@@ -143,13 +131,11 @@ def naiveReconstruction(points, normals, X, Y, Z):
     point (x,y,z)
     Args:
         points :  points of the point cloud
-		normals:  normals of the point cloud
-		X,Y,Z  :  coordinates of grid vertices 
+                normals:  normals of the point cloud
+                X,Y,Z  :  coordinates of grid vertices 
     Returns:
         IF     : implicit function sampled at the grid points
     """
-    print(X.shape,points.shape, normals.shape, points[0]-[1,3,4])
-
 
     ################################################
     # <================START CODE<================>
@@ -159,92 +145,43 @@ def naiveReconstruction(points, normals, X, Y, Z):
     IF = np.random.rand(X.shape[0], X.shape[1], X.shape[2]) - 0.5
     import time
 
-
-    # for _ in range(X.shape[0]):
-    #     # print(_)
-    #     t = time.localtime()
-    #     # print
-
-    #     current_time = time.strftime("%H:%M:%S", t)
-    #     print(_,current_time)
-    #     for __ in range(X.shape[0]):
-    #         tempval = 10000000
-    #         ans = 100000
-    #         for ___ in range(X.shape[0]):
-    #             x = X[_][__][___]
-    #             y = Y[_][__][___]
-    #             z = Z[_][__][___]
-    #             p = [x,y,z]
-                
-    #             # for idx in range(len(points)):
-    #             #     point = points[idx]
-    #             #     tv = np.linalg.norm(p-point)
-    #             #     if( tv < tempval):
-    #             #         tempval = tv
-    #             #         ans = tv*(normals[idx]@(p-point))
-    #         IF[_][__][___] = ans
-
-
-
-
-    # this is an example of a kd-tree nearest neighbor search (adapt it accordingly for your task)
-	# use kd-trees to find nearest neighbors efficiently!
-	# kd-tree: https://en.wikipedia.org/wiki/K-d_tree
-
-    shapeX = X.shape    
+    shapeX = X.shape
     Q = np.array([X.reshape(-1), Y.reshape(-1), Z.reshape(-1)]).transpose()
-    print(Q.shape)
-    print("**********",Q.shape)
-    print(X,Q)
     tree = KDTree(points)
 
-    _, idx = tree.query(Q, k=1)  
-    # print(idx,idx.reshape(shapeX))
-    idxs = idx.reshape(shapeX)
-    for _ in range(len(idxs)):
-        for __ in range(len(idxs)):
-            for ___ in range(len(idxs)):
-                x = X[_][__][___]
-                y = Y[_][__][___]
-                z = Z[_][__][___]
-                p = [x,y,z]
-                idxt = idxs[_][__][___]
-                point = points[idxt]
-                tv = np.linalg.norm(p-point)
-                tv =1 
-                IF[_][__][___] = tv*(normals[idxt]@(p-point))
+    _, idx = tree.query(Q, k=1)
 
+    pts = points[idx]
+    nrmls = normals[idx]
+    IF = IF.reshape(-1)
+    for i, pointQ in enumerate(Q):
+        IF[i] = nrmls[i]@(pointQ-pts[i])[0]
+    IF = IF.reshape(shapeX)
 
-
-    # print(_,idx)
-
-	
     ################################################
     # <================END CODE<================>
     ################################################
 
-    return IF 
-
-
+    return IF
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Basic surface reconstruction')
-    parser.add_argument('--file', type=str, default = "sphere.pts", help='input point cloud filename')
-    parser.add_argument('--method', type=str, default = "sphere",\
+    parser = argparse.ArgumentParser(
+        description='Basic surface reconstruction')
+    parser.add_argument('--file', type=str, default="sphere.pts",
+                        help='input point cloud filename')
+    parser.add_argument('--method', type=str, default="sphere",
                         help='method to use: mls (Moving Least Squares), naive (naive reconstruction), sphere (just shows a sphere)')
     args = parser.parse_args()
 
-    print("---------------------------",args)
-
-    #load the point cloud
+    # load the point cloud
     data = np.loadtxt(args.file)
 
     points = data[:, :3]
     normals = data[:, 3:6]
 
     # create grid whose vertices will be used to sample the implicit function
-    X,Y,Z,max_dimensions,min_dimensions = createGrid(points, 64)
+    X, Y, Z, max_dimensions, min_dimensions = createGrid(points, 64)
 
     if args.method == 'mls':
         print(f'Running Moving Least Squares reconstruction on {args.file}')
@@ -256,8 +193,8 @@ if __name__ == '__main__':
         # toy implicit function of a sphere - replace this code with the correct
         # implicit function based on your input point cloud!!!
         print(f'Replacing point cloud {args.file} with a sphere!')
-        center =  (max_dimensions + min_dimensions) / 2
-        R = max( max_dimensions - min_dimensions ) / 4
-        IF =  sphere(center, R, X, Y, Z)
+        center = (max_dimensions + min_dimensions) / 2
+        R = max(max_dimensions - min_dimensions) / 4
+        IF = sphere(center, R, X, Y, Z)
 
     showMeshReconstruction(IF)
